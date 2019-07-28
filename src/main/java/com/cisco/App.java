3ba3cc6d71895d734a8de9056219fcb762e42f69
hongwzh2 <421690794@qq.com>
2019-07-28 12:59:09 +0800

    first commit

---
+++ src/main/java/com/cisco/App.java
@@ -0,0 +1,13 @@
+package com.cisco;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
